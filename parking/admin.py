37c549ddb95ff08a16446b6703c5479692d8a60d
lleofernandes <lf.04@hotmail.com>
2025-03-24 22:56:52 -0300

    feat(uat): manager admin parking

--- parking/admin.py
+++ parking/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from parking.models import ParkingSpot, ParkingRecord
 
-# Register your models here.
+@admin.register(ParkingSpot)
+class ParkingSpotAdmin(admin.ModelAdmin):
+    list_display = ['spot_number', 'is_occupeid']
+    search_fields = ['spot_number']
+    list_filter = ['is_occupeid']
+
+
+@admin.register(ParkingRecord)
+class ParkingRecordAdmin(admin.ModelAdmin):
+    list_display = ['vehicle', 'parking_spot', 'entry_time', 'exit_time']
+    search_fields = ['vehicle__licence_plate', 'parking_spot__spot_number']    
